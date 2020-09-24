200d614158263655d9219677ca01d5571187accb
Levi <levi-sr@outlook.com>
2020-09-23 22:43:14 -0300

    Added the user1 image template

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
-import {Link} from 'react-router-dom'
+
+import user1 from './img/user-1.jpg'
 
 const App = () => {
   return (
     <div className="ui comment">
       <div className="comment">
         <a href="/" className='avatar'>
-          <img alt="Profile Picture"></img>
+          <img src={user1} alt="Profile" style={{width: '200px'}}></img>
         </a>
         <div className="content">
           <a href="/" className="author">
